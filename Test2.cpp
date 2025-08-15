5efc29666f0fbd90053c51a64b0b38243c196a71
JustMek <165890939+JustMek7@users.noreply.github.com>
2025-08-15 17:54:05 +0700

    Update Test2.cpp

--- Test2.cpp
+++ Test2.cpp
@@ -4,7 +4,7 @@ int main(){
     int n1,n2,max;
     cin >> n1; //รับ input แรก
     max =n1; //ตั้ง max ตรงกับ input แรกก่อน
-    for(int i=0;i>=2;i++){ //loop input 2 ครั้งเฉยๆตรงนี้
+    for(int i=0;i>2;i++){ //loop input 2 ครั้งเฉยๆตรงนี้
     cin >> n2;
     if(n2>max){ //ถ้า input ล่าสุดมากกว่า input แรก จะตั้งค่า max ให้เป็น input ล่าสุด
         max=n2;
@@ -13,4 +13,5 @@ int main(){
     cout << max;
 
     return 0;
+
 }
